0255e2ea906db69c93b3cb67e1240ef948fea01a
David Beckley <beckl.ds@gmail.com>
2015-08-20 16:51:04 -0700

    Add users route

--- server/users/userRoutes.js
+++ server/users/userRoutes.js
@@ -4,4 +4,5 @@ module.exports = function (app) {
   app.post('/signup', userController.signup);
   app.post('/signin', userController.signin);
   app.post('/profile', userController.getUser);
+  app.get('/users', userController.getAll);
 };
